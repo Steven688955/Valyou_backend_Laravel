39be4e19fa39a7ec003a41f0001d86f2ec7b89b5
Steven688955 <superdev128@gmail.com>
2023-11-23 22:24:35 -0800

    fix the issue of UrlGenerator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,12 +23,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        // if (env('APP_ENV') == 'production') {
-        //     $url->forceScheme('https');
-        // }
-
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+        
         Schema::defaultStringLength(191);
     }
 }
